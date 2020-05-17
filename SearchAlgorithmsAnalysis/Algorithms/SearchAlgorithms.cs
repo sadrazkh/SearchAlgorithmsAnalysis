c57dd6451a0150df5a8d6f678b3a7bf2b711deb1
Saleh Kiani <kiani.mohammadsaleh@gmail.com>
2020-05-17 13:18:33 +0430

    Mahmood Functions and new Interpolation Added

--- SearchAlgorithmsAnalysis/Algorithms/SearchAlgorithms.cs
+++ SearchAlgorithmsAnalysis/Algorithms/SearchAlgorithms.cs
@@ -21,8 +21,20 @@ namespace Algorithms
 
             //string res = $"{time.Hours:00}:{time.Minutes:00}:{time.Seconds:00}.{time.Milliseconds:00}";
             return time.Milliseconds;
+
         }
 
+
+
+        private static int linear_search(string element, List<string> namesList, int low, int high)
+        {
+            int i = low;
+            while (low < high && namesList[i] != element)
+                i++;
+            if (namesList[i - 1].Equals(element))
+                return i - 1;
+            return 0;
+        }
 
 
         public static int InterpolationSearch2(string element, List<string> namesList)
@@ -101,12 +113,157 @@ namespace Algorithms
         }
 
 
+        public static int InterpolationSearch_new(string element, List<string> namesList)
+        {
+            int first = (element.StartsWith('a')) ? 0 : Percentages(namesList)[(int)element[0] - 97 - 1];
+            int last = Percentages(namesList)[(int)element[0]-97];
+            return linear_search(element, namesList , first , last);
+        }
+
+
+
         private static int StringDiff(string s1, string s2, int num)
         {
             int diff = 0;
             for (int i = 0; i < num; i++)
                 diff += (s1[i] - s2[i])* (int)Math.Pow(10,num-1);
             return diff;
+        }
+
+        private static List<int> Percentages(List<string> a)
+        {
+            List<int> result = new List<int>(new int[26]);
+            foreach(var element in a)
+            {
+                result[(int)element[0] - 97]++;
+            }
+            for(int i=0;i<26;i++)
+            {
+                if (i != 0)
+                    result[i] += result[i - 1];
+            }
+            return result;
+        }
+
+
+
+        //Mahmood Functions
+        public static int binarynnorecursive(int low, int high, List<string> s, string x, int n)
+        {
+            while (low <= high)
+            {
+                if (x == s[low])
+                    return low;
+                if (x == s[high])
+                    return high;
+                int mid, lengh, i;
+                int[] d = new int[2];
+                lengh = (high - low + 1);
+                if (lengh < n)
+                    n = 2;
+                mid = lengh / n;
+                d[1] = low;
+                for (i = 1; i < n; i++)
+                {
+                    d[0] = d[1];
+                    d[1] = i * mid;
+                    if (x == s[d[1]])
+                    {
+                        return d[1];
+                    }
+                    else if (x.CompareTo(s[d[1]]) < 0)
+                    {
+                        low = d[0] + 1;
+                        high = d[1] - 1;
+                        break;
+                    }
+                }
+                if (i == n)
+                {
+                    low = d[1] + 1;
+                    high = high - 1;
+                }
+            }
+            return 0;
+        }
+
+
+
+        public static int Binaryn(int low, int high, List<string> s, string x, int n)
+        {
+
+            if (low > high)
+                return 0;
+            if (x == s[low])
+                return low;
+            if (x == s[high])
+                return high;
+            int mid, lengh, i;
+            int[] d = new int[2];
+            lengh = (high - low + 1);
+            if (lengh < n)
+                n = 2;
+            mid = lengh / n;
+            d[1] = low;
+            for (i = 1; i < n; i++)
+            {
+                d[0] = d[1];
+                d[1] = i * mid;
+                if (x == s[d[1]])
+                {
+                    return d[1];
+                }
+                else if (x.CompareTo(s[d[1]]) < 0)
+                {
+                    return Binaryn(d[0] + 1, d[1] - 1, s, x, n);
+                }
+                return 0;
+            }
+            if (i == n)
+            {
+                return Binaryn(d[1] + 1, high - 1, s, x, n);
+            }
+            return 0;
+        }
+
+
+        public static int combination(string x, List<string> s, int low, int high, int n, int m)
+        {
+            while (high - low > m)
+            {
+                if (x == s[low])
+                    return low;
+                if (x == s[high])
+                    return high;
+                int mid, lengh, i;
+                int[] d = new int[2];
+                lengh = (high - low + 1);
+                if (lengh <= n)
+                    n = 2;
+                mid = lengh / n;
+                d[1] = low;
+                for (i = 1; i < n; i++)
+                {
+                    d[0] = d[1];
+                    d[1] = i * mid;
+                    if (x == s[d[1]])
+                    {
+                        return d[1];
+                    }
+                    else if (x.CompareTo(s[d[1]]) < 0)
+                    {
+                        low = d[0] + 1;
+                        high = d[1] - 1;
+                        break;
+                    }
+                }
+                if (i == n)
+                {
+                    low = d[1] + 1;
+                    high = high - 1;
+                }
+            }
+            return linear_search(x, s, low, high);
         }
     }
 };
--- SearchAlgorithmsAnalysis/Main/Program.cs
+++ SearchAlgorithmsAnalysis/Main/Program.cs
@@ -12,12 +12,12 @@ namespace Main
             var watch = new System.Diagnostics.Stopwatch();
             watch.Start();
             var t = GetFakeData.GetAll();
-
+            Console.WriteLine(SearchAlgorithms.InterpolationSearch_new("farid",t));
             //foreach (var item in t)
             //    Console.WriteLine(item);
 
-            Console.WriteLine("LinearSearch:" + SearchAlgorithms.LinearSearch("zoe.wright", t) + " ms");
-            Console.WriteLine("InterpolationSearch2:" + SearchAlgorithms.InterpolationSearch2("zoe.wright", t) + " ms");
+            //Console.WriteLine("LinearSearch:" + SearchAlgorithms.LinearSearch("zoe.wright", t) + " ms");
+            //Console.WriteLine("InterpolationSearch2:" + SearchAlgorithms.InterpolationSearch2("zoe.wright", t) + " ms");
             //Console.WriteLine("Interpolation Average:" + Tests.InterAveragetime(t));
             //watch.Stop();
             //TimeSpan times = watch.Elapsed;
